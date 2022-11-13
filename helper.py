ff69aa7c22cbfb93c039fb81737c5a252f5969d1
Nagy Noel <nagynooel@gmail.com>
2022-11-13 17:31:19 +0100

    Created redirect_alert function and alert style.

--- helper.py
+++ helper.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 
 # -- Error handling
 # Render error to user
 def error(message, code=400):
     return render_template("error.html", e_code = code, message = message), code
+
+
+# -- Functions
+# - Global
+# Redirect user with an alert
+def redirect_alert(redirect_to: str, alert_msg:str, alert_type="danger"):
+    flash(alert_msg, "alert-" + alert_type)
+    return redirect(redirect_to)
